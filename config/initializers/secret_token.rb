3684a3de3eff6822d346977b0c37f543937cf774
榎本 悠介 <mosa@mosamba.local>
2012-06-23 16:04:58 +0900

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scaf::Application.config.secret_token = '62e0ba9ada3bcc4d7d98ef4dad315acb9f1a2ad0445174cb636eff68af7389d020609669f63e7263c5799f4530a4c65e27948a67f8c6e2fd72bb202530d120ce'
